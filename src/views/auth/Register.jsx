29b4d25eb7a20896abfdcfdfe08bfe8b4f3ad7a9
NOMAN1802 <mustakimnoman82@gmail.com>
2024-03-21 00:39:01 +0600

    register and login page

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {AiOutlineGooglePlus, AiOutlineGithub} from 'react-icons/ai'
+import {FiFacebook} from 'react-icons/fi'
+import {CiTwitter} from 'react-icons/ci'
 
 const Register = () => {
+
+    const [state, setState] = useState({
+
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const inputHandel = (e) =>{
+        setState ({
+            ...state,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const submit = (e) =>{
+        e.preventDefault()
+        console.log(state);
+    }
     return (
         <div className='min-w-screen min-h-screen bg-[#161d31] flex justify-center items-center'>
 
@@ -14,18 +37,18 @@ const Register = () => {
                         Please register to your account and start your business
                     </p>
 
-                    <form>
+                    <form onSubmit={submit}>
                         <div className='flex flex-col w-full gap-1 mb-3'>
                             <label htmlFor='name'>Name</label>
-                            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden'  type='text' name='name' placeholder='name' id='name' required/>
+                            <input onChange={inputHandel} value={state.name} className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden'  type='text' name='name' placeholder='name' id='name' required/>
                         </div>
                         <div className='flex flex-col w-full gap-1 mb-3'>
                             <label htmlFor='email'>Email</label>
-                            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden'  type='text' name='email' placeholder='email' id='email' required/>
+                            <input onChange={inputHandel} value={state.email} className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden'  type='email' name='email' placeholder='email' id='email' required/>
                         </div>
                         <div className='flex flex-col w-full gap-1 mb-3'>
                             <label htmlFor='password'>Password</label>
-                            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden'  type='password' name='password' placeholder='password' id='password' required/>
+                            <input onChange={inputHandel} value={state.password} className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden'  type='password' name='password' placeholder='password' id='password' required/>
                         </div>
                         <div className='flex items-center w-full gap-3 mb-3'>
                             
@@ -33,6 +56,38 @@ const Register = () => {
                             <label htmlFor='checkbox'> I agree to privacy policy & terms </label>
                         </div>
                         <button className='bg-blue-500 w-full hover:shadow-blue-500/50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>Sign Up</button>
+                          <div className='flex justify-center item-center mb-3 gap-3'>
+                            <p>Already have an account ? <Link to={'/login'}>Signin here</Link></p>
+                          </div>
+                        <div className='flex items-center justify-center mb-3'>
+
+                            <div className='w-[45%] bg-slate-700 h-[1px]'>
+
+                            </div>
+                            <div className='w-[10%] justify-center items-center'>
+                                <span className='pb-1'> Or </span>
+                            </div>
+                            <div className='w-[45%] bg-slate-700 h-[1px]'>
+
+                            </div>
+                        </div>
+
+                        <div className='flex justify-center items-center gap-3'>
+                            <div className=' w-[35px] h-[35px] flex rounded-md bg-orange-700 shadow-lg cursor-pointer hover:shadow-orange-700/50 justify-center items-center overflow-hidden'>
+                            <span> <AiOutlineGooglePlus/></span>
+                            </div>
+                            <div className=' w-[35px] h-[35px] flex rounded-md bg-indigo-700 shadow-lg cursor-pointer hover:shadow-orange-700/50 justify-center items-center overflow-hidden'>
+                            <span> <FiFacebook/></span>
+                            </div>
+                            <div className=' w-[35px] h-[35px] flex rounded-md bg-cyan-700 shadow-lg cursor-pointer hover:shadow-cyan-700/50 justify-center items-center overflow-hidden'>
+                            <span> <CiTwitter/></span>
+                            </div>
+                            <div className=' w-[35px] h-[35px] flex rounded-md bg-purple-700 shadow-lg cursor-pointer hover:shadow-purple-700/50 justify-center items-center overflow-hidden'>
+                            <span> <AiOutlineGithub/></span>
+                            </div>
+                            
+                            
+                        </div>
                     </form>
 
                 </div>
